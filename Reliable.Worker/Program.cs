bb0f7e5329a7c08a55871502fc2639d1a09e0c28
Roberto Carrillo <email@robertocarrillo.com>
2022-12-28 14:46:16 -0600

    Flatten Program

--- Reliable.Worker/Program.cs
+++ Reliable.Worker/Program.cs
@@ -3,107 +3,99 @@ using Reliable.Messages.Commands;
 using Reliable.Messages.Events;
 using System.Diagnostics;
 using Reliable.Domain;
+using Reliable.Worker;
 using Serilog;
 using Serilog.Events;
 
-namespace Reliable.Worker
-{
-    public class Program
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
     {
-        public static void Main(string[] args)
+        services.AddSingleton<Inventory>(_ =>
         {
-            Log.Logger = new LoggerConfiguration()
-                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
-                .Enrich.FromLogContext()
-                .WriteTo.Console()
-                .CreateBootstrapLogger();
+            var inventory = new Inventory();
+            inventory.GenerateRandomItems();
+            return inventory;
+        });
+        services.AddHostedService<Worker>();
+    })
+    .UseSerilog((context, services, configuration) => configuration
+        .ReadFrom.Configuration(context.Configuration)
+        .ReadFrom.Services(services)
+        .Enrich.FromLogContext()
+        .WriteTo.Console())
+    .ConfigureLogging(logger => logger.AddConsole())
+    .UseConsoleLifetime()
+    .UseNServiceBus(ctx =>
+    {
+        // TODO: consider moving common endpoint configuration into a shared project
+        // for use by all endpoints in the system
 
-            IHost host = Host.CreateDefaultBuilder(args)
-                .ConfigureServices(services =>
-                {
-                    services.AddSingleton<Inventory>(_ =>
-                    {
-                        var inventory = new Inventory();
-                        inventory.GenerateRandomItems();
-                        return inventory;
-                    });
-                    services.AddHostedService<Worker>();
-                })
-                .UseSerilog((context, services, configuration) => configuration
-                    .ReadFrom.Configuration(context.Configuration)
-                    .ReadFrom.Services(services)
-                    .Enrich.FromLogContext()
-                    .WriteTo.Console())
-                .ConfigureLogging(logger => logger.AddConsole())
-                .UseConsoleLifetime()
-                .UseNServiceBus(ctx =>
-                {
-                    // TODO: consider moving common endpoint configuration into a shared project
-                    // for use by all endpoints in the system
+        // TODO: give the endpoint an appropriate name
+        var endpointConfiguration = new EndpointConfiguration("Inventory");
 
-                    // TODO: give the endpoint an appropriate name
-                    var endpointConfiguration = new EndpointConfiguration("Inventory");
+        // TODO: ensure the most appropriate serializer is chosen
+        // https://docs.particular.net/nservicebus/serialization/
+        endpointConfiguration.UseSerialization<NewtonsoftJsonSerializer>();
+        endpointConfiguration.DefineCriticalErrorAction(OnCriticalError);
 
-                    // TODO: ensure the most appropriate serializer is chosen
-                    // https://docs.particular.net/nservicebus/serialization/
-                    endpointConfiguration.UseSerialization<NewtonsoftJsonSerializer>();
-                    endpointConfiguration.DefineCriticalErrorAction(OnCriticalError);
+        endpointConfiguration.Conventions()
+            .DefiningCommandsAs(t => t == typeof(IncreaseInventory) || t == typeof(DecreaseInventory))
+            .DefiningEventsAs(t => t == typeof(InventoryUpdated));
 
-                    endpointConfiguration.Conventions()
-                        .DefiningCommandsAs(t => t == typeof(IncreaseInventory) || t == typeof(DecreaseInventory))
-                        .DefiningEventsAs(t => t == typeof(InventoryUpdated));
+        // TODO: remove this condition after choosing a transport, persistence and deployment method suitable for production
+        if (Environment.UserInteractive && Debugger.IsAttached)
+        {
+            // TODO: choose a durable transport for production
+            // https://docs.particular.net/transports/
+            var transport = endpointConfiguration.UseTransport<RabbitMQTransport>();
+            transport.UseConventionalRoutingTopology(QueueType.Quorum);
+            transport.ConnectionString("host=localhost");
 
-                    // TODO: remove this condition after choosing a transport, persistence and deployment method suitable for production
-                    if (Environment.UserInteractive && Debugger.IsAttached)
-                    {
-                        // TODO: choose a durable transport for production
-                        // https://docs.particular.net/transports/
-                        var transport = endpointConfiguration.UseTransport<RabbitMQTransport>();
-                        transport.UseConventionalRoutingTopology(QueueType.Quorum);
-                        transport.ConnectionString("host=localhost");
+            // TODO: choose a durable persistence for production
+            // https://docs.particular.net/persistence/
+            endpointConfiguration.UsePersistence<LearningPersistence>();
 
-                        // TODO: choose a durable persistence for production
-                        // https://docs.particular.net/persistence/
-                        endpointConfiguration.UsePersistence<LearningPersistence>();
-
-                        // TODO: create a script for deployment to production
-                        endpointConfiguration.EnableInstallers();
-                    }
-
-                    // TODO: replace the license.xml file with your license file
-
-                    return endpointConfiguration;
-                })
-                .Build();
-            host.Run();
+            // TODO: create a script for deployment to production
+            endpointConfiguration.EnableInstallers();
         }
 
-        private static async Task OnCriticalError(ICriticalErrorContext context, CancellationToken cancellationToken)
-        {
-            // TODO: decide if stopping the endpoint and exiting the process is the best response to a critical error
-            // https://docs.particular.net/nservicebus/hosting/critical-errors
-            try
-            {
-                await context.Stop(cancellationToken);
-            }
-            finally
-            {
-                FailFast($"Critical error, shutting down: {context.Error}", context.Exception);
-            }
-        }
+        // TODO: replace the license.xml file with your license file
 
-        private static void FailFast(string message, Exception exception)
-        {
-            try
-            {
-                // TODO: decide what kind of last resort logging is necessary
-                // TODO: when using an external logging framework it is important to flush any pending entries prior to calling FailFast
-                // https://docs.particular.net/nservicebus/hosting/critical-errors#when-to-override-the-default-critical-error-action
-            }
-            finally
-            {
-                Environment.FailFast(message, exception);
-            }
-        }
+        return endpointConfiguration;
+    })
+    .Build();
+host.Run();
+
+static async Task OnCriticalError(ICriticalErrorContext context, CancellationToken cancellationToken)
+{
+    // TODO: decide if stopping the endpoint and exiting the process is the best response to a critical error
+    // https://docs.particular.net/nservicebus/hosting/critical-errors
+    try
+    {
+        await context.Stop(cancellationToken);
+    }
+    finally
+    {
+        FailFast($"Critical error, shutting down: {context.Error}", context.Exception);
+    }
+}
+
+static void FailFast(string message, Exception exception)
+{
+    try
+    {
+        // TODO: decide what kind of last resort logging is necessary
+        // TODO: when using an external logging framework it is important to flush any pending entries prior to calling FailFast
+        // https://docs.particular.net/nservicebus/hosting/critical-errors#when-to-override-the-default-critical-error-action
+    }
+    finally
+    {
+        Environment.FailFast(message, exception);
     }
 }
